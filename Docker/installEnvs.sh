6be699e98b19ca758d8a42dca0c31f607b0a31b0
Jan Svatoñ <janek@dhcp157-095.ceitec.muni.cz>
2024-03-11 15:21:13 +0100

    switch to conda env create

--- Docker/installEnvs.sh
+++ Docker/installEnvs.sh
@@ -1,12 +1,10 @@
-conda create \
---file envs/bcftools.yml \
---file envs/bedtools.yml \
---file envs/biopet.yml \
---file envs/bwa.yml \
---file envs/cutadapt.yml \
---file envs/erko.yml \
---file envs/py36.yml \
---file envs/samtools.yml \
---file envs/vardict.yml \
---file envs/varscan.yml \
---file envs/vep.yml && conda clean --all -f -y
+#!/bin/bash
+
+# Loop through all .yml files in the current directory
+for file in *.yml; do
+    # Check if the file exists
+    if [ -f "$file" ]; then
+        # Execute the conda command to create environment from the YAML file
+        mamba env create -f "$file"
+    fi
+done
