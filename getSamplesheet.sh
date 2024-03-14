3dae0573db138547d7462f451465fd82a4f8d39a
Jan Svatoñ <janek@dhcp157-095.ceitec.muni.cz>
2024-03-14 14:02:08 +0100

    add java env

--- getSamplesheet.sh
+++ getSamplesheet.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Download the CSV file from a URL
-wget "https://docs.google.com/spreadsheets/d/1jDX2pudze8Bk_DxtwtOrKYF-oBXHCzjPtykY5JY-6bI/export?format=csv" -O "samplesheet.csv"
+wget "https://docs.google.com/spreadsheets/d/1WOktQDMH13d_zr0g8be1BqysMd7TIyy4LrzPywfvjhA/export?format=csv" -O "samplesheet.csv"
 current_dir=$(pwd)
 
 # Convert the CSV file to JSON using a Python script
-python $current_dir/project/xsvato01/mareckova_CXCR_k8s/src/CsvToJson.py $current_dir/samplesheet.csv $current_dir/samplesheet.json
+python $current_dir/project/xsvato01/TP53_nf/scripts/CsvToJson.py $current_dir/samplesheet.csv $current_dir/samplesheet.json
+#rm samplesheet.csv
